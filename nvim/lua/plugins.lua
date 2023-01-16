c4634fddb9f494a074ab22fd4ac7099cea4ab696
kedama-t <114888622+kedama-t@users.noreply.github.com>
2023-01-16 18:47:22 +0900

    switch nvim-lsp to coc.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,115 +1,63 @@
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(
-  function()
-    -- plugin manager
-    use 'wbthomason/packer.nvim'
-
-    -- LSP Support
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/nvim-lsp-installer'
-
-    -- Autocompletion
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'saadparwaiz1/cmp_luasnip'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-nvim-lua'
-
-    -- Snippets
-    use 'L3MON4D3/LuaSnip'
-    use 'rafamadriz/friendly-snippets'
-
-    use 'nvim-lua/lsp-status.nvim'
-
-    use {
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-        require("trouble").setup {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        }
-      end
-    }
-
+vim.cmd("packadd vim-jetpack")
+require("jetpack.packer").add {
+    {"tani/vim-jetpack", opt = 1},
     -- outline
-    use {
-      'stevearc/aerial.nvim',
-      config = function() require('aerial').setup() end
-    }
+    {
+        "stevearc/aerial.nvim",
+        config = function()
+            require("aerial").setup()
+        end
+    },
+    -- devicons
+    "kyazdani42/nvim-web-devicons",
     -- indent
-    use "lukas-reineke/indent-blankline.nvim"
-
-    -- fuzzy finder
-    use { 'junegunn/fzf', run = './install --bin', }
-    use { 'ibhagwan/fzf-lua',
-      -- optional for icon support
-      requires = { 'kyazdani42/nvim-web-devicons' }
-    }
-
-    -- treesitter
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use { 'nvim-treesitter/nvim-treesitter-textobjects' }
-
-    use({
-      "kylechui/nvim-surround",
-      tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-      config = function()
-        require("nvim-surround").setup({
-        })
-      end
-    })
-
+    "lukas-reineke/indent-blankline.nvim",
+    -- finder
+    "junegunn/fzf.vim",
+    {"junegunn/fzf", run = "call fzf#install()"},
+    "ibhagwan/fzf-lua",
+    -- lsp
+    {"neoclide/coc.nvim", branch = "release"},
+        -- treesitter
+    {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"},
     -- appearance
-    use 'sainnhe/gruvbox-material'
-    use { 'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-
+    "sainnhe/gruvbox-material",
+    -- status bar
+    {
+        "nvim-lualine/lualine.nvim",
+    },
     -- tagbar
-    use { 'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons' }
-
+    {"akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons"},
     -- filer
-    use {
-      'kyazdani42/nvim-tree.lua',
-      requires = {
-        'kyazdani42/nvim-web-devicons', -- optional, for file icon
-      },
-      tag = 'nightly' -- optional, updated every week. (see issue #1193)
-    }
-
+    {
+        "kyazdani42/nvim-tree.lua",
+        tag = "nightly" -- optional, updated every week. (see issue #1193)
+    },
     -- utils
-    use 'ap/vim-css-color'
-
+    "ap/vim-css-color",
+    "machakann/vim-sandwich",
+    "alvan/vim-closetag",
     -- comment
-    use {
-      'numToStr/Comment.nvim',
-      config = function()
-        require('Comment').setup()
-      end
-    }
-
-    -- code formatter
-    use { 'prettier/vim-prettier', run = { "yarn install --frozen-lockfile --production" } }
-
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end
+    },
+        -- code formatter
+    {"prettier/vim-prettier", run = {"yarn install --frozen-lockfile --production"}},
     -- jump
-    use {
-      'phaazon/hop.nvim',
-      branch = 'v1', -- optional but strongly recommended
-      config = function()
-        -- you can configure Hop the way you like here; see :h hop-config
-        require 'hop'.setup { keys = 'asdfjkl;qweruiop' }
-      end
-    }
-
+    {
+        "phaazon/hop.nvim",
+        branch = "v1", -- optional but strongly recommended
+        config = function()
+            -- you can configure Hop the way you like here; see :h hop-config
+            require "hop".setup {keys = "asdfjkl;qweruiop"}
+        end
+    },
     -- Git
-    use 'tpope/vim-fugitive'
+    "tpope/vim-fugitive",
+    -- diagnostics
+    {"folke/trouble.nvim",config=function() require("trouble").setup() end}
 
-    -- Flutter
-    use 'dart-lang/dart-vim-plugin'
-    use 'thosakwe/vim-flutter'
-  end)
+  }
