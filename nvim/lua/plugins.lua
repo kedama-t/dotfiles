b623f40f0e5334f01b603e3ef39b7c0bee09c7d8
Takeda.M <114888622+kedama-t@users.noreply.github.com>
2024-01-11 17:44:50 +0900

    テーマ変更

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,6 +9,7 @@ return {
     'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
+      require('rose-pine').setup({ variant = "moon" })
       cmd([[colorscheme rose-pine]])
     end,
   },
